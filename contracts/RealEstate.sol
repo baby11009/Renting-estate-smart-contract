b1fdca42543ce5e7c7d5b8d7f82d813feb11b51c
baby11009 <vohuythanh2003@gmail.com>
2025-05-12 20:25:59 +0700

    update

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -16,9 +16,15 @@ contract HopDongThueCanHo {
         uint256 thoiDiemKetThuc;
     }
 
+    struct DeNghiGiaHan {
+        uint256 soThangThem;
+        uint256 soTienDaChuyen;
+    }
+
     ThongTinNha public thongTinNha;
     ThongTinThue private thongTinThue;
-    mapping(address => uint256) public soDuTaiKhoan;
+    DeNghiGiaHan private deNghiGiaHan;
+    mapping(address => uint256) private soDuTaiKhoan;
 
     constructor(uint256 _chiPhiHangThang, uint256 _thoiGianThueThang) {
         require(_chiPhiHangThang > 0, "Chi phi hang thang phai > 0");
@@ -57,7 +63,7 @@ contract HopDongThueCanHo {
         if (block.timestamp < thongTinThue.thoiDiemKetThuc) {
             thoiGianConLaiTinh =
                 (thongTinThue.thoiDiemKetThuc - block.timestamp) /
-                10 seconds;
+                30 seconds;
         }
 
         return (
@@ -83,13 +89,13 @@ contract HopDongThueCanHo {
         thongTinThue.thoiDiemKetThuc =
             block.timestamp +
             thongTinNha.thoiGianThueThang *
-            10 seconds;
+            30 seconds;
 
         soDuTaiKhoan[thongTinThue.nguoiThue] = msg.value;
         thongTinNha.hopDongDuocKy = true;
     }
 
-    function rutTien(uint256 soThangMuonRut) external {
+    function rutTien(uint256 soThangMuonRut) external payable {
         require(
             msg.sender == thongTinNha.chuNha,
             "Chi chu nha moi duoc rut tien"
@@ -101,7 +107,7 @@ contract HopDongThueCanHo {
         );
 
         uint256 soThangDaThue = (block.timestamp -
-            thongTinThue.thoiDiemBatDau) / 10 seconds;
+            thongTinThue.thoiDiemBatDau) / 30 seconds;
 
         uint256 soThangChoRut = soThangDaThue;
 
@@ -117,19 +123,59 @@ contract HopDongThueCanHo {
         );
 
         uint256 soTienRut = soThangMuonRut * thongTinNha.chiPhiHangThang;
-        require(
-            soDuTaiKhoan[thongTinThue.nguoiThue] >= soTienRut,
-            "So du khong du"
-        );
 
         soDuTaiKhoan[thongTinThue.nguoiThue] -= soTienRut;
-        soDuTaiKhoan[thongTinNha.chuNha] += soTienRut;
-        thongTinThue.soThangDaThanhToan += soThangMuonRut;
 
-        payable(thongTinNha.chuNha).transfer(soTienRut);
+        thongTinThue.soThangDaThanhToan += soThangMuonRut;
+        (bool success, ) = payable(thongTinNha.chuNha).call{value: soTienRut}(
+            ""
+        );
+
+        require(success, "Chuyen tien that bai");
     }
 
-    function giaHanHopDong(uint256 soThangThem) external payable {
+    function xemSoDu(address taiKhoan) external view returns (uint256) {
+        require(
+            msg.sender == thongTinThue.nguoiThue ||
+                msg.sender == thongTinNha.chuNha,
+            "Chi nguoi thue hoac chu nha moi duoc xem so du"
+        );
+
+        return soDuTaiKhoan[taiKhoan];
+    }
+
+    function rutSoDu(uint256 soTienCanRut) external payable {
+        require(
+            msg.sender == thongTinThue.nguoiThue,
+            "Chi nguoi thue moi duoc rut so du"
+        );
+        require(soTienCanRut > 0, "So tien can rut > 0");
+
+        uint256 soTienDuocRut = soDuTaiKhoan[msg.sender];
+        uint256 soTienChuaThanhToan = (thongTinNha.thoiGianThueThang -
+            thongTinThue.soThangDaThanhToan) * thongTinNha.chiPhiHangThang;
+        soTienDuocRut = soTienDuocRut - soTienChuaThanhToan;
+        if (
+            deNghiGiaHan.soTienDaChuyen > 0 &&
+            deNghiGiaHan.soThangThem * thongTinNha.chiPhiHangThang <
+            deNghiGiaHan.soTienDaChuyen
+        ) {
+            uint256 soTienCanPhaiBu = deNghiGiaHan.soThangThem *
+                thongTinNha.chiPhiHangThang -
+                deNghiGiaHan.soTienDaChuyen;
+
+            soTienDuocRut = soTienDuocRut - soTienCanPhaiBu;
+
+            deNghiGiaHan.soTienDaChuyen += soTienCanPhaiBu;
+        }
+        require(soTienDuocRut >= soTienCanRut, "So du khong du de thuc hien");
+
+        (bool success, ) = payable(msg.sender).call{value: soTienDuocRut}("");
+        require(success, "Chuyen tien that bai");
+        soDuTaiKhoan[thongTinThue.nguoiThue] = soTienChuaThanhToan;
+    }
+
+    function yeuCauGiaHan(uint256 soThangThem) external payable {
         require(thongTinNha.hopDongDuocKy, "Chua co hop dong");
         require(
             msg.sender == thongTinThue.nguoiThue,
@@ -137,7 +183,7 @@ contract HopDongThueCanHo {
         );
         // Giới hạn thời gian gia hạn: chỉ được phép gia hạn trong vòng 5s sau khi hết hạn
         require(
-            block.timestamp <= thongTinThue.thoiDiemKetThuc + 5 seconds,
+            block.timestamp <= thongTinThue.thoiDiemKetThuc + 15 seconds,
             "Da qua thoi gian cho phep gia han"
         );
 
@@ -152,17 +198,64 @@ contract HopDongThueCanHo {
             "So du con lai khong du de gia han"
         );
 
-        // Gia hạn thời gian kết thúc hợp đồng
-        thongTinThue.thoiDiemKetThuc += soThangThem * 10 seconds;
-
-        // Cập nhật tổng số tháng trong hợp đồng
-        thongTinNha.thoiGianThueThang += soThangThem;
-
-        // Cập nhật số dư tài khoản
-        soDuTaiKhoan[msg.sender] += msg.value;
+        deNghiGiaHan = DeNghiGiaHan({
+            soThangThem: soThangThem,
+            soTienDaChuyen: msg.value
+        });
     }
 
-    function chamDutHopDong() external {
+    function xemYeuCauGiaHanHopDong()
+        external
+        view
+        returns (uint256 soThangThem, uint256 soTienDaChuyen)
+    {
+        require(thongTinNha.hopDongDuocKy, "Chua co hop dong");
+        require(
+            msg.sender == thongTinThue.nguoiThue ||
+                msg.sender == thongTinNha.chuNha,
+            "Chi nguoi thue hoac chu nha moi duoc xem yeu cau gia han"
+        );
+        require(deNghiGiaHan.soThangThem > 0, "Chua co yeu cau gia han");
+
+        return (deNghiGiaHan.soThangThem, deNghiGiaHan.soTienDaChuyen);
+    }
+
+    function chapNhanYeuCauGiaHan() external {
+        require(thongTinNha.hopDongDuocKy, "Chua co hop dong");
+        require(
+            msg.sender == thongTinNha.chuNha,
+            "Chi chu nha moi duoc chap nhan gia han"
+        );
+        require(deNghiGiaHan.soThangThem > 0, "Chua co yeu cau gia han");
+
+        // Gia hạn thời gian kết thúc hợp đồng
+        thongTinThue.thoiDiemKetThuc += deNghiGiaHan.soThangThem *30 seconds;
+
+        // Cập nhật tổng số tháng trong hợp đồng
+        thongTinNha.thoiGianThueThang += deNghiGiaHan.soThangThem;
+
+        // Cập nhật số dư tài khoản
+        soDuTaiKhoan[thongTinThue.nguoiThue] += deNghiGiaHan.soTienDaChuyen;
+
+        delete deNghiGiaHan;
+    }
+
+    function huyYeuCauGiaHan() external payable {
+        require(thongTinNha.hopDongDuocKy, "Chua co hop dong");
+        require(
+            msg.sender == thongTinNha.chuNha ||
+                msg.sender == thongTinThue.nguoiThue,
+            "Chi chu nha hoac nguoi thue moi duoc huy gia han"
+        );
+        require(deNghiGiaHan.soThangThem > 0, "Chua co yeu cau gia han");
+        (bool success, ) = payable(msg.sender).call{
+            value: deNghiGiaHan.soTienDaChuyen
+        }("");
+        require(success, "Hoan tien that bai");
+        delete deNghiGiaHan;
+    }
+
+    function chamDutHopDong() external payable {
         require(thongTinNha.hopDongDuocKy, "Chua co hop dong");
         require(
             msg.sender == thongTinThue.nguoiThue ||
@@ -170,32 +263,39 @@ contract HopDongThueCanHo {
             "Chi chu nha hoac nguoi thue moi co quyen"
         );
 
-        uint256 tongThangThue = thongTinNha.thoiGianThueThang;
-        uint256 soThangDaSuDung = (block.timestamp -
-            thongTinThue.thoiDiemBatDau) / 10 seconds;
+        if (soDuTaiKhoan[thongTinThue.nguoiThue] > 0) {
+            uint256 tongThangThue = thongTinNha.thoiGianThueThang;
+            uint256 soThangDaSuDung = (block.timestamp -
+                thongTinThue.thoiDiemBatDau) / 30 seconds;
 
-        if (soThangDaSuDung > tongThangThue) {
-            soThangDaSuDung = tongThangThue;
+            if (soThangDaSuDung > tongThangThue) {
+                soThangDaSuDung = tongThangThue;
+            }
+
+            uint256 soTienChuaThanhToan = (soThangDaSuDung -
+                thongTinThue.soThangDaThanhToan) * thongTinNha.chiPhiHangThang;
+
+            uint256 tienHoanLai = soDuTaiKhoan[thongTinThue.nguoiThue] -
+                soTienChuaThanhToan;
+
+            // Nếu có tiền hoàn lại, thực hiện chuyển tiền
+            if (tienHoanLai > 0) {
+                (bool success, ) = payable(msg.sender).call{value: tienHoanLai}(
+                    ""
+                );
+                require(success, "Chuyen tien that bai");
+            }
+
+            // CHuyển số tiền chưa thanh toán cho chủ nhà
+            if (soTienChuaThanhToan > 0) {
+                (bool success, ) = payable(msg.sender).call{
+                    value: soTienChuaThanhToan
+                }("");
+                require(success, "Chuyen tien that bai");
+            }
+
+            soDuTaiKhoan[thongTinThue.nguoiThue] = 0;
         }
-
-        uint256 soTienChuaThanhToan = (soThangDaSuDung -
-            thongTinThue.soThangDaThanhToan) * thongTinNha.chiPhiHangThang;
-
-        uint256 tienHoanLai = soDuTaiKhoan[thongTinThue.nguoiThue] -
-            soTienChuaThanhToan;
-
-        // Nếu có tiền hoàn lại, thực hiện chuyển tiền
-        if (tienHoanLai > 0) {
-            (bool success, ) = payable(msg.sender).call{value: tienHoanLai}("");
-            require(success, "Chuyen tien that bai");
-        }
-
-        // CHuyển số tiền chưa thanh toán cho chủ nhà
-        if (soTienChuaThanhToan > 0) {
-            soDuTaiKhoan[thongTinNha.chuNha] += soTienChuaThanhToan;
-        }
-
-        soDuTaiKhoan[thongTinThue.nguoiThue] = 0;
 
         // Chấm dứt hợp đồng
         thongTinNha.hopDongDuocKy = false;
